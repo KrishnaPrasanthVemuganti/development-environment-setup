0df5ece1957ed24b5e8e5a565d4985bacfa8fbf7
Krishna Prasanth Vemuganti <share2kris@gmail.com>
2021-01-14 20:56:25 +0530

    Update initialSetup.sh

--- windows/initialSetup.sh
+++ windows/initialSetup.sh
@@ -122,6 +122,24 @@ do
     esac
 done
 
+echo -n "Do you want to installdocker and docker-compose(Y/n):"
+read docker
+if [ $docker == "Y" ]
+then
+	# Install Gatsby
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+fi
+
 echo "Installing zsh"
 sudo apt install zsh
 
@@ -139,4 +157,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 echo "Setting default shell to zsh"
 chsh -s $(which zsh)
 
-echo "All set!!! Please restart your terminal"
+echo "All set!!! Please restart your WSL, from power shell run: wslconfig /t Ubuntu"
